1bb357c6aba4f4518f2d7954adbcdc529b34f830
lou <dustyfingers01257@gmail.com>
2024-10-31 16:10:31 -0600

    style snippet detail page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -11,5 +11,18 @@ export default async function ViewSnippetPage(props: ViewSnippetPageProps) {
   const { id } = props.params;
   const snippet = await db.snippet.findFirst({ where: { id: parseInt(id) } });
   if (!snippet) return notFound();
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">edit</button>
+          <button className="p-2 border rounded">delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
